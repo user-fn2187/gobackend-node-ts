6ce39413715e0e241b021c8a80c2ed3d8920ef2b
FelipeRodrigues1337 <proen_dev@hotmail.com>
2021-01-25 13:28:15 -0300

    v00

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333,()=>{
+    console.log(' 🔥 Back-end Started on port 3333! 🔥 ');
+})
